ac177e031a3ff93f38792a6efe173a85d5fd59e6
JoshM2624 <joshm2624@gmail.com>
2019-06-12 17:53:01 -0500

    hangman

---
+++ 06week/hangman.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+var word = 'Hello';
+var arr = word.split('');
+var solved = ['_','_','_','_','_'];
+var maxLives = 5;
+var right = 0;
+var wrong = 0;
+var letter = '';
+
+function getPrompt() {
+    
+    rl.question('which letter?: ', (letter) => {
+      
+        hangman(letter);
+        wrongLetter();
+        console.log(solved);
+        checkForWin(arr, solved);
+        if(wrong < 5){
+            getPrompt();
+        }
+        
+        }
+        
+      
+
+  
+    )}
+    
+
+function checkForWin(arr, solved){
+        if(arr === solved){
+            console.log("YOU WIN YOU STUD");
+
+
+        }
+        else if(wrong > 5){
+            console.log("GAME OVER SUCKER");
+    }
+}
+
+
+
+
+function hangman(letter){
+
+//Loop through the word and find the letter that matches the prompt
+    for (let i=0;i<arr.length;i++){
+
+        if(letter === arr[i] ){
+          solved[i] = letter;
+          right++;
+          console.log(right);
+        }
+        
+        }
+        
+    
+}
+function wrongLetter(){
+    if (hangman(letter) == false){
+        wrong++;
+        console.log(wrong);
+
+    }
+}
+getPrompt();
+
+ 
+
+
+
+
